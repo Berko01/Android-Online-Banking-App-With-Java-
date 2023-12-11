01d55e85647ad0763e34334f80eda4facbb7129d
Celal Karahan <celal51kara@gmail.com>
2023-12-11 15:24:06 +0300

    [Feat] Register&Login pages
    Kayıt&Giriş sayfaları tasarlandı, logo eklendi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "com.example.onlinebankingappproject"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
---
+++ app/src/main/java/com/example/onlinebankingappproject/view/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.example.onlinebankingappproject.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+import com.example.onlinebankingappproject.R;
+import com.example.onlinebankingappproject.api.ApiAuthService;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView signUpText = findViewById(R.id.sign_up_text);
+
+        signUpText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Burada RegisterActivity'ye geçiş yapılacak
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        // XML dosyasındaki EditText ve Button elemanlarını tanımlama
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        // Giriş yapma butonuna tıklanınca çağrılacak fonksiyonu belirleme
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // EditText elemanlarından e-posta ve şifre bilgilerini alıp değişkenlere atama
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+                // API servisini oluştur
+                ApiAuthService apiAuthService = new ApiAuthService();
+                apiAuthService.login(email,password);
+
+            }
+        });
+    }
+}
